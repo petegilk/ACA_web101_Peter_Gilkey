b56e477bd24430582e6b860b5c3bacc9dbe7f4a4
peter gilkey <peter_gilkey@yahoo.com>
2020-03-04 03:54:20 -0600

    added js counter, reset button

---
+++ src/checkpoint3/checkpoint3.js
@@ -0,0 +1,28 @@
+//declaring variables by ID
+var currentCount = 0;
+var countElement = document.getElementById("counter")
+var reset = document.getElementById("resetButton")
+console.log(countElement.innerText)
+
+//setting count to zero by default
+if (countElement.innerText === "") {
+  countElement.innerText = 0;
+}
+
+function addByOne() {
+  currentCount++
+  countElement.innerText = currentCount
+  console.log("working", currentCount);
+  //
+  if (currentCount === 10) {
+    alert("Ten");
+  }
+}
+
+function resetCounter() {
+  countElement.innerText = ""
+  if (countElement.innerText === "") {
+    countElement.innerText = 0;
+    currentCount = 0;
+  }
+}
