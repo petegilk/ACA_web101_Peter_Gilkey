9403f44309a17bc74d20c25972877fff223a2b60
peter gilkey <peter_gilkey@yahoo.com>
2020-03-03 18:40:04 -0600

    ttt

--- src/ticTacToe/ttt.js
+++ src/ticTacToe/ttt.js
@@ -1,6 +1,7 @@
 //user clicks square
 // 'x' or 'o' is added to square
 
+
 function addGamePiece (selectedElement) {
   console.log('selectedElement', selectedElement)
 
@@ -11,10 +12,11 @@ function addGamePiece (selectedElement) {
   //add text to element
   xo.innerText = "X"
   selectedElement.appendChild(xo)
+  previousPlay = "X"
 
   if (previousPlay === "X") {
     xo.innerText === "O"
-  
+    selectedElement.appendChild(xo)
   } else if (selectedElement.innerText === "O") {
     selectedElement.innerText === null
   }
