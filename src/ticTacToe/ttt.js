cd2847e4d08ee703e888e3d6801649066add1987
peter gilkey <peter_gilkey@yahoo.com>
2020-03-06 02:52:31 -0600

    ttt - towers

--- src/ticTacToe/ttt.js
+++ src/ticTacToe/ttt.js
@@ -1,41 +1,42 @@
-//user clicks square
-// 'x' or 'o' is added to square
 
 
-function addGamePiece (selectedElement) {
-  console.log('selectedElement', selectedElement)
-  var previousPlay;
 
-  //create element
-  var xo = document.createElement('h1')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addGamePiece (selectedElement) {
+//   console.log('selectedElement', selectedElement)
+//   var previousPlay;
+
+//   //create element
+//   var xo = document.createElement('h1')
+//   xo.innerText === ""
   
-  //add text to element
-  if (xo.innerText === "") {
-    xo.innerText === "X"
-    selectedElement.appendChild(xo)
-  } else if (xo.innerText === "X") {
-    xo.innerText === "O"
-    selectedElement.appendChild(xo)
-  } else if (xo.innerText === "O") {
-    xo.innerText === ""
-    selectedElement.appendChild(xo)
-  }
-
+//   //add text to element
+//   if (xo.innerText === "") {
+//     xo.innerText === "X"
+//   } else if (xo.innerText === "X") {
+//     xo.innerText === "O"
+//   } else if (xo.innerText === "O") {
+//     xo.innerText === ""
+//   }
+//   selectedElement.appendChild(xo)
 
   // xo.innerText = "X"
   // selectedElement.appendChild(xo)
   // previousPlay = "X"
 
-
-
-
-
-
-
-  //   if (previousPlay === "X") {
-//     xo.innerText === "O"
-//     selectedElement.appendChild(xo)
-//   } else if (selectedElement.innerText === "O") {
-//     selectedElement.innerText === null
-//   }
-// }
